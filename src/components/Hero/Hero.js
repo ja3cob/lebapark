250470596f0626359eec96cedb1e0c9299a6cbd3
ja3cob <jakubecker@gmail.com>
2025-02-13 21:05:41 +0100

    Revert "Dim video with react intersection observer"
    This reverts commit cd5c52e62d5619d3f5df2e3244caf3b113c01179.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,35 +2,17 @@ import Logo from "../../assets/logo.png";
 import Covid from "../../assets/hero.png";
 import Video from "../../assets/hero.mp4";
 import { Scene } from "react-scrollmagic";
-import { useInView } from "react-intersection-observer";
-import { useEffect, useRef, useState } from "react";
 
 const Hero = ({ HeroRef }) => {
-  const videoRef = useRef(null);
-  const [logoYPositionValue, setLogoYPositionValue] = useState(9999);
-  const {ref, entry} = useInView({threshold: Array.from(Array(101).keys()).map(i => i / 100)});
-  const [shouldUpdateBrightness, setShouldUpdateBrightness] = useState(true);
-
-  useEffect(() => {
-    setShouldUpdateBrightness(logoYPositionValue > 0);
-  }, [logoYPositionValue]);
-
-  useEffect(() => {
-    if (!entry || !shouldUpdateBrightness) {
-      return;
-    }
-
-    videoRef.current.style.filter = `brightness(${Math.max((1 - entry.intersectionRatio) * 100, 30)}%)`;
-  }, [entry, shouldUpdateBrightness]);
   return (
     <div className="hero-container">
-        <div className="hero-video" ref={videoRef}>
+        <div className="hero-video">
           <video autoPlay loop muted>
             <source src={Video} type="video/mp4" />
           </video>
         </div>
         <div className="hero-before-logo" ref={HeroRef}></div>
-        <div className="hero-logo" ref={element => {ref(element); setLogoYPositionValue(element?.getBoundingClientRect().top);}}>
+        <div className="hero-logo">
           <Scene duration={853} classToggle={[".nav-1", "active"]}>
             <img src={Logo} alt="Logo parku dinozaurów Łeba" />
           </Scene>
