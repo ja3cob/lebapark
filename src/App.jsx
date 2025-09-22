6a57e1396d8b65ed20c98c92177a0d8232bcb7c5
ja3cob <jakubecker@gmail.com>
2025-09-22 18:24:11 +0200

    Replace whole site with under construction communicate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Logo from "./assets/logo.png";
+
+export default function App() {
+  return (
+    <div className="under-construction">
+      <div className="hero-logo">
+        <img src={Logo} alt="Logo parku dinozaurów Łeba" />
+      </div>
+      <h1>Strona w budowie</h1>
+    </div>
+  );
+}
